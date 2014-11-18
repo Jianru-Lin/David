506a494d599a023a2a93522074eee313e9901047
Jianru Lin <jianru.lin@gmail.com>
2014-11-18 12:02:57 +0800

    we can upload files now

--- david/routes/publish.js
+++ david/routes/publish.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res) {
   res.render('publish');
 });
 
+router.post('/', function(req, res) {
+	console.log(req.body)
+	console.log(req.files)
+})
+
 module.exports = router;
